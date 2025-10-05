d361741dd6f8170c8dfbe93f7e81dcc0090f0fb3
Lucas McGill <lucasmcgill@lucass-mbp.mynetworksettings.com>
2025-10-05 16:59:46 -0400

    change to debug block and first materials

--- globals/paths.gd
+++ globals/paths.gd
@@ -1,4 +1,7 @@
 extends Node
 
+func get_material_path(material_name : String):
+	return 'res:///assets/materials/' + material_name + '.tres'
+
 func get_dialog_path(character_name : String, dialog_name : String, tree_name := "index"):
 	return 'res://scenes/characters/npcs/' + character_name + '/dialog/' + dialog_name + '/' + tree_name + '.tres'
