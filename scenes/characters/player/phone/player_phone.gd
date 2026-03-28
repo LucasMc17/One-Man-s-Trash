2d4e92ed2046a8a46f6dfcc82f87e5bcd50eb7e7
Lucas McGill <lmlucasmc8@gmail.com>
2026-03-28 14:03:11 -0400

    refactored 3d ui and phone scene

--- scenes/characters/player/phone/player_phone.gd
+++ scenes/characters/player/phone/player_phone.gd
@@ -1,25 +1,23 @@
+class_name Phone
 extends MeshInstance3D
+## The Player's phone as a 3d scene.
 
-# EXPORTS
-@export_group("Phone State")
-@export var y_position := -0.19
-@export var y_rotation := 90.0:
+## The Y position which the phone should move toward in local space.
+var _y_position := -0.19
+## The Y rotation which the phone should pivot toward in local space.
+var _y_rotation := 90.0:
 	get():
-		return deg_to_rad(y_rotation)
-@export var active := false
+		return deg_to_rad(_y_rotation)
 
-# FUNCS
 func activate():
-	active = true
-	y_position = 0.0
-	y_rotation = 0.0
+	_y_position = 0.0
+	_y_rotation = 0.0
 
 func deactivate():
-	active = false
-	y_position = -0.19
-	y_rotation = 90.0
+	_y_position = -0.19
+	_y_rotation = 90.0
 
 # BUILT INS
 func _process(_delta):
-	position.y = lerp(position.y, y_position, 0.1)
-	rotation.y = lerp(rotation.y, y_rotation, 0.1)
+	position.y = lerp(position.y, _y_position, 0.1)
+	rotation.y = lerp(rotation.y, _y_rotation, 0.1)
