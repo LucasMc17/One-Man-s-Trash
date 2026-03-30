ef42a6baff88e00a652b0109d59c784725132610
Lucas McGill <lmlucasmc8@gmail.com>
2026-03-29 20:49:37 -0400

    setup for automatic migration

--- globals/migration.gd
+++ globals/migration.gd
@@ -1,8 +1,6 @@
 @tool
 extends EditorScript
 
-var NPCs = [ResourceLoader.load('res://scenes/characters/npcs/bart/bart.tscn')]
-
 func _run():
 	var npc : NPC = EditorInterface.get_edited_scene_root()
 	print("migrating ", npc.name)
